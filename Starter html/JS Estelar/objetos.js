e2cf14f38d660610e738fc37a81b7ad7bd1e16fe
Digital <alexandregomesdefarias@gmail.com>
2023-01-26 12:51:22 -0300

    cont. curso. js

---
+++ Starter html/JS Estelar/objetos.js
@@ -0,0 +1,15 @@
+// Criando objeto com valores e propriedades: 
+const person = {
+    name: 'John',
+    age: 30,
+    weight: 88.6,
+    isAdmin: true
+}
+
+// a pessoa John tem 30 anos e pesa 88.6.
+
+console.log(`$ A pessoa ${person.name} tem ${person.age} ano e pesa ${person.weight}.`)
+
+// Ela é admin do sistema?
+
+console.log(person.isAdmin == true)
